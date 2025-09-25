da3381b2c1e35a436c62d4456da568b96ae6b9c8
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2025-09-25 21:40:57 +0700

    feat(actions): add news server actions
    - Add src/actions/news.ts with server actions for CRUD operations on news,
    
      including attachment management and publishing/unpublishing
    
    - Update src/actions/index.ts to export news actions in server object

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 import { announcement } from "./announcement";
 import { directory } from "./directory";
-import { tag } from "./tag";
+import { news } from "./news";
 import { person } from "./person";
+import { tag } from "./tag";
 
-export const server = { announcement, directory, tag, person };
+export const server = { announcement, directory, news, tag, person };
