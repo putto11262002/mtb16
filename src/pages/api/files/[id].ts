45963afba97644719a65ed3bd0f666bf01b0cf89
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2025-09-26 23:04:53 +0700

    feat(storage): add Netlify file storage implementation
    - Introduced singleton FileStore instance for managing local and Netlify storage.
    - Added NetlifyFileStore with methods for file management and metadata handling.
    - Implemented stream conversion utilities for compatibility with Node.js and web streams.
    - Updated API route to serve files using the new Netlify storage methods.

--- src/pages/api/files/[id].ts
+++ src/pages/api/files/[id].ts
@@ -1,38 +1,8 @@
 import { getFileStore } from "@/lib/storage";
+import { nodejsReadableStreamToWebReadableStream } from "@/lib/utils/stream";
 import type { APIRoute } from "astro";
-import type { Readable } from "node:stream";
 const storage = getFileStore();
 export const prerender = false;
-
-export const nodejsReadableStreamToWebReadableStream = (
-  node: Readable,
-): ReadableStream => {
-  return new ReadableStream({
-    start(controller) {
-      // https://streams.spec.whatwg.org/#example-rs-push-backpressure
-      node.on("data", (chunk) => {
-        controller.enqueue(chunk);
-        if (!controller.desiredSize) {
-          node.pause();
-        }
-      });
-      node.on("end", () => {
-        controller.close();
-      });
-      node.on("error", (err) => {
-        controller.error(err);
-      });
-    },
-    pull() {
-      if (node.isPaused()) {
-        node.resume();
-      }
-    },
-    cancel() {
-      node.destroy();
-    },
-  });
-};
 
 export const GET: APIRoute = async ({ params, request }) => {
   const id = params.id!;
